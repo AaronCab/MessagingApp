1c915c4a19796e6ec07efd47c8a5d2befed33004
AaronCab <aaroncabreja@pursuit.org>
2020-01-17 11:30:25 -0500

    Initial Commit

---
+++ MessagingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by Aaron Cabreja on 1/17/20.
+//  Copyright © 2020 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
